b5a94a4a4af975ca13298fcb486c295a1bccd0bc
mymikasa <1342627550@qq.com>
2021-04-21 06:57:25 -0700

    Add 118

---
+++ Go/118_pascals-triangle.go
@@ -0,0 +1,44 @@
+package main
+
+func generate(numRows int) [][]int {
+	res := make([][]int, numRows)
+
+	for i := range res {
+		res[i] = make([]int, i+1)
+		res[i][0] = 1
+		res[i][i] = 1
+
+		for j := 1; j < i; j++ {
+			res[i][j] = res[i-1][j] + res[i-1][j-1]
+		}
+	}
+
+	return res
+}
+
+// func getRow(rowIndex int) []int {
+
+// 	res := make([][]int, rowIndex)
+
+// 	for i := range res {
+// 		res[i] = make([]int, i+1)
+// 		res[i][0] = 1
+// 		res[i][i] = 1
+
+// 		for j := 1; j < i; j++ {
+// 			res[i][j] = res[i-1][j] + res[i-1][j-1]
+// 		}
+// 	}
+
+// 	return res[rowIndex]
+
+// }
+
+func getRow(rowIndex int) []int {
+	row := make([]int, rowIndex+1)
+	row[0] = 1
+	for i := 1; i <= rowIndex; i++ {
+		row[i] = row[i-1] * (rowIndex - i + 1) / i
+	}
+	return row
+}
