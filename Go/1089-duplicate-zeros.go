5722dc3de1b9b928785588fcb3aaa9f85c1b3ff6
mikasa <mikasa5012@gmail.com>
2022-06-17 18:10:27 +0800

    add 168 & 1089

---
+++ Go/1089-duplicate-zeros.go
@@ -0,0 +1,57 @@
+package main
+
+//[1,0,2,3,0,4,5,0]
+//[1,0,0,2,3,0,0,4]
+
+func duplicateZeros(arr []int) {
+	r := 0
+	lenth := len(arr)
+	ar := 0
+
+	for i, v := range arr {
+		r++
+		if v == 0 {
+			r++
+		}
+		if r >= lenth {
+			ar = i
+			break
+		}
+	}
+
+	// 最后一个值为0，且不能重复
+	if r > lenth {
+		r -= 2
+		arr[r] = arr[ar]
+		r--
+	} else {
+		r--
+		if arr[ar] == 0 {
+			arr[r] = arr[ar]
+			arr[r-1] = arr[ar]
+			r -= 2
+		} else {
+			arr[r] = arr[ar]
+			r -= 1
+		}
+	}
+
+	// if r > lenth {
+	// 	r--
+	// }
+
+	// if r == lenth {
+	// 	r--
+	// 	arr[r] = arr[ar]
+	// 	r--
+	// }
+
+	for j := ar - 1; j >= 0; j-- {
+		if arr[j] == 0 {
+			arr[r] = 0
+			r--
+		}
+		arr[r] = arr[j]
+		r--
+	}
+}
---
+++ Go/168-excel-sheet-column-title.go
@@ -0,0 +1,14 @@
+package main
+
+func convertToTitle(columnNumber int) string {
+	ans := []byte{}
+
+	for columnNumber > 0 {
+		temp := (columnNumber-1)%26 + 1
+		ans = append(ans, 'A'+byte(temp-1))
+		columnNumber = (columnNumber - temp) / 26
+	}
+	return string(ans)
+}
+
+//
