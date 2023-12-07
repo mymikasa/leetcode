ed24465e434c9cafcf18f9835e1c496b3c6135e6
mikasa <mikasa5012@gmail.com>
2023-12-07 23:22:17 +0800

    完成1466

--- daily-question/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/main.go
+++ daily-question/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/main.go
@@ -2,37 +2,35 @@ package main
 
 func minReorder(n int, connections [][]int) int {
 
-	// e := make([][][]int, n)
-
 	e := make(map[int][][]int)
-
 	known := make(map[int]bool)
-
 	for i := 0; i < n; i++ {
 		known[i] = false
 	}
-
-	// 联通图
+	// 建图
 	for _, v := range connections {
 		e[v[0]] = append(e[v[0]], []int{v[1], 1})
 		e[v[1]] = append(e[v[1]], []int{v[0], 0})
 	}
 
 	var dfs func(int)
-
 	res := 0
 	dfs = func(start int) {
-
+		if known[start] {
+			return
+		}
 		known[start] = true
 		for i, v := range e[start] {
-			if v[1] == 1 {
-				res += 1
-				e[start][i][1] = 0
-			}
 			if !known[v[0]] {
 				dfs(v[0])
+			} else {
+				if v[1] == 0 {
+					res += 1
+					e[start][i][1] = 0
+				}
 			}
 		}
 	}
+	dfs(0)
 	return res
 }
--- daily-question/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/main_test.go
+++ daily-question/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/main_test.go
@@ -12,7 +12,22 @@ func Test_minReorder(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				n:           6,
+				connections: [][]int{{0, 1}, {1, 3}, {2, 3}, {4, 0}, {4, 5}},
+			},
+			want: 3,
+		},
+		{
+			name: "case2",
+			args: args{
+				n:           5,
+				connections: [][]int{{1, 0}, {1, 2}, {3, 2}, {3, 4}},
+			},
+			want: 2,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
